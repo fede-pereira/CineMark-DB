886df57f496bdecba84837420bb70bd78fce6c3f
florianreyes <florianreyes541@gmail.com>
2024-05-09 22:35:40 -0300

    Crear queries

--- cine-database-docker/cargar_datos_desde_csv.sql
+++ cine-database-docker/cargar_datos_desde_csv.sql
@@ -1,3 +1,98 @@
+-- COPY actores (
+--     id_actor, 
+--     nombre, 
+--     apellido, 
+--     edad, 
+--     genero
+-- )
+-- FROM '/data/actores.csv'
+-- DELIMITER ';'
+-- CSV HEADER
+-- --para indicar que la primera lı́nea es el encabezado
+-- ENCODING 'LATIN1';
+
+-- COPY actua (
+--     nombre_pelicula, 
+--     director, 
+--     id_actor, 
+--     salario
+-- )
+-- FROM '/data/actua.csv'
+-- DELIMITER ';'
+-- CSV HEADER
+-- --para indicar que la primera lı́nea es el encabezado
+-- ENCODING 'LATIN1';
+
+-- COPY clientes (
+--     cuit, 
+--     nombre, 
+--     apellido, 
+--     edad,
+--     nacionalidad,
+--     genero
+-- )
+-- FROM '/data/clientes.csv'
+-- DELIMITER ';'
+-- CSV HEADER
+-- --para indicar que la primera lı́nea es el encabezado
+-- ENCODING 'LATIN1';
+
+-- COPY compras (
+--     id_compra, 
+--     nombre_pelicula, 
+--     director, 
+--     id_sala,
+--     id_cine,
+--     ts,
+--     cuit,
+--     cantidad
+-- )
+-- FROM '/data/compras.csv'
+-- DELIMITER ';'
+-- CSV HEADER
+-- --para indicar que la primera lı́nea es el encabezado
+-- ENCODING 'LATIN1';
+
+-- COPY funciones (
+--     nombre_pelicula, 
+--     director, 
+--     id_sala,
+--     id_cine,
+--     ts
+-- )
+-- FROM '/data/funciones.csv'
+-- DELIMITER ';'
+-- CSV HEADER
+-- --para indicar que la primera lı́nea es el encabezado
+-- ENCODING 'LATIN1';
+
+-- COPY peliculas (
+--     nombre_pelicula, 
+--     director, 
+--     duracion_en_segundos,
+--     estudio,
+--     presupuesto,
+--     tiempo_de_realizacion,
+--     ano_estreno,
+--     genero_pelicula
+-- )
+-- FROM '/data/peliculas.csv'
+-- DELIMITER ';'
+-- CSV HEADER
+-- --para indicar que la primera lı́nea es el encabezado
+-- ENCODING 'LATIN1';
+
+-- COPY salas (
+--     id_sala, 
+--     id_cine, 
+--     asientos
+-- )
+-- FROM '/data/salas.csv'
+-- DELIMITER ';'
+-- CSV HEADER
+-- --para indicar que la primera lı́nea es el encabezado
+-- ENCODING 'LATIN1';
+
 COPY actores (
     id_actor, 
     nombre, 
@@ -6,6 +101,22 @@ COPY actores (
     genero
 )
 FROM '/data/actores.csv'
+DELIMITER ';'
+CSV HEADER
+--para indicar que la primera lı́nea es el encabezado
+ENCODING 'LATIN1';
+
+COPY peliculas (
+    nombre_pelicula, 
+    director, 
+    duracion_en_segundos,
+    estudio,
+    presupuesto,
+    tiempo_de_realizacion,
+    ano_estreno,
+    genero_pelicula
+)
+FROM '/data/peliculas.csv'
 DELIMITER ';'
 CSV HEADER
 --para indicar que la primera lı́nea es el encabezado
@@ -37,17 +148,12 @@ CSV HEADER
 --para indicar que la primera lı́nea es el encabezado
 ENCODING 'LATIN1';
 
-COPY compras (
-    id_compra, 
-    nombre_pelicula, 
-    director, 
-    id_sala,
-    id_cine,
-    ts,
-    cuit,
-    cantidad
+COPY salas (
+    id_sala, 
+    id_cine, 
+    asientos
 )
-FROM '/data/compras.csv'
+FROM '/data/salas.csv'
 DELIMITER ';'
 CSV HEADER
 --para indicar que la primera lı́nea es el encabezado
@@ -66,29 +172,20 @@ CSV HEADER
 --para indicar que la primera lı́nea es el encabezado
 ENCODING 'LATIN1';
 
-COPY peliculas (
+
+COPY compras (
+    id_compra, 
     nombre_pelicula, 
     director, 
-    duracion_en_segundos,
-    estudio,
-    presupuesto,
-    tiempo_de_realizacion,
-    ano_estreno,
-    genero_pelicula
+    id_sala,
+    id_cine,
+    ts,
+    cuit,
+    cantidad
 )
-FROM '/data/peliculas.csv'
+FROM '/data/compras.csv'
 DELIMITER ';'
 CSV HEADER
 --para indicar que la primera lı́nea es el encabezado
 ENCODING 'LATIN1';
 
-COPY salas (
-    id_sala, 
-    id_cine, 
-    asientos
-)
-FROM '/data/salas.csv'
-DELIMITER ';'
-CSV HEADER
---para indicar que la primera lı́nea es el encabezado
-ENCODING 'LATIN1';
---
+++ cine-database-docker/queries.sql
@@ -0,0 +1,41 @@
+-- Ganancias totales por genero de peliculas
+SELECT p.genero_pelicula, SUM(c.cantidad) * 4000 AS ingresos_totales
+FROM compras c
+JOIN funciones f ON c.id_sala = f.id_sala AND c.id_cine = f.id_cine AND c.ts = f.ts
+JOIN peliculas p ON c.nombre_pelicula = p.nombre_pelicula AND c.director = p.director
+GROUP BY p.genero_pelicula;
+   
+-- Edad promedio de clientes por genero de peliculas   
+SELECT p.genero_pelicula, AVG(c.edad) AS avg_age
+FROM compras cm
+JOIN funciones f ON cm.id_sala = f.id_sala AND cm.id_cine = f.id_cine AND cm.ts = f.ts
+JOIN peliculas p ON cm.nombre_pelicula = p.nombre_pelicula AND cm.director = p.director
+JOIN clientes c ON cm.cuit = c.cuit
+GROUP BY p.genero_pelicula;
+
+-- Las 5 peliculas con mas ganancia total
+SELECT nombre_pelicula, director, ingresos_totales
+FROM (SELECT c.nombre_pelicula, c.director,
+        SUM(cantidad) * 4000 AS ingresos_totales,
+        ROW_NUMBER() OVER (ORDER BY SUM(cantidad) * 4000 DESC) AS RANK
+        FROM compras c
+        JOIN funciones f ON c.id_sala = f.id_sala AND c.id_cine = f.id_cine AND c.ts = f.ts
+    GROUP BY
+        c.nombre_pelicula, c.director
+) AS ranked_movies
+WHERE rank <= 5;
+
+-- Porcentaje y cantidad de clientes por rango etario
+WITH rango_etario AS (
+SELECT
+CASE
+    WHEN edad BETWEEN 0 AND 18 THEN '0-18'
+    WHEN edad BETWEEN 19 AND 30 THEN '19-30'
+    WHEN edad BETWEEN 31 AND 45 THEN '31-45'
+    WHEN edad BETWEEN 46 AND 60 THEN '46-60'
+    ELSE 'Above 60'
+END AS grupo_etario, COUNT(*) AS cantidad
+FROM clientes
+GROUP BY grupo_etario)
+SELECT grupo_etario, cantidad, ROUND(count::numeric / SUM(cantidad) OVER () * 100, 2) AS porcentaje
+FROM rango_etario;
